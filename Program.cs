36920d19272faf64448dfc501b17d26dce2fc782
Akashkapase9988 <akashkapase9988@gmail.com>
2021-09-19 18:34:27 +0530

    completed first five and remaining NUNIT testing

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+
+namespace day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // stopwatch.Stopwatch();
+            //    reverse.numreverse();
+            perfectdivisor.perfectnum();
+            prime.primenum();
+            fibonacci.fseries();
+            
+          
+        }
+    }
+    }
+
---
+++ stopwatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+namespace day6
+{
+    class stopwatch
+    {
+        public static void Stopwatch() {
+            Console.WriteLine("Hello World!");
+            // Create new stopwatch.
+            Stopwatch stopwatch = new Stopwatch();
+
+            // Begin timing.
+            stopwatch.Start();
+
+            // Do something.
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(1);
+            }
+
+            // Stop timing.
+            stopwatch.Stop();
+
+            // Write result.
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+        }
+    }
+}
